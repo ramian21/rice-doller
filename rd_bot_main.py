c5a8c62fe996266547f8078f065406952ce0f84b
Ramon Antonio <netsurfer2132@gmail.com>
2021-02-25 18:54:07 -0500

    Add a custom bot status to show an example help message command.

--- rd_bot_main.py
+++ rd_bot_main.py
@@ -153,6 +153,7 @@ def format_log(entries, banner):
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='!rd help'))
     print('We have logged in as {0.user}'.format(client))
 
 
